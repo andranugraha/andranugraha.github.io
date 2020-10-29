ab3919804ccef2d3b547c42051ad785a78103d6c
andranugraha <andranugraha@users.noreply.github.com>
2020-10-29 21:02:47 +0700

    Updated

---
+++ portfolio.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+    $(window).scroll(function(){
+        var scroll = $(window).scrollTop();
+        if (scroll > 700) {
+            $("nav").css("top" , "0px");
+            $("nav").css("opacity" , "1");
+        } else {
+            $("nav").css("top" , "-50px");
+        }
+        dh = $(document).height();
+        wh = $(window).height();
+        var height = (scroll / (dh-wh)) * 60;
+        var top = 50 - (scroll / (dh-wh)) * 30;
+        $(".scrollBar").css("height" , height + "vh");
+        $(".scrollBar").css("top" , top + "vh");
+    })
+})
