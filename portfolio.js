bf0f018cba11a43644ff192439ebdedb1ece244a
andranugraha <andranugraha@users.noreply.github.com>
2020-11-01 19:01:50 +0700

    tes

--- portfolio.js
+++ portfolio.js
@@ -1,3 +1,7 @@
+$(window).load(function() {
+	$(".loader").delay(2000).fadeOut("slow");
+  $("#overlayer").delay(2000).fadeOut("slow");
+})
 $(document).ready(function(){
     $(window).scroll(function(){
         var scroll = $(window).scrollTop();
@@ -13,5 +17,5 @@ $(document).ready(function(){
         var top = 50 - (scroll / (dh-wh)) * 30;
         $(".scrollBar").css("height" , height + "vh");
         $(".scrollBar").css("top" , top + "vh");
-    })
-})
+    });
+});
